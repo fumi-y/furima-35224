58f6f60f1b79b7947b5221c8992ed1c85d1199fd
fumi-y <0901fumito@gmail.com>
2021-04-16 11:45:06 +0900

    itemモデルの作成&アソシエーションの記述

---
+++ app/models/item.rb
@@ -0,0 +1,24 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_day
+  belongs_to :postage
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    with_options numericality: { other_than: 0 } do
+      validates :condition_id
+      validates :postage_id
+      validates :area_id
+      validates :delivery_day_id
+      validates :category_id
+    end
+  end
+end
+
