d1cf4d855ff949f420a1661c57a2ec22d82b12ef
fumi-y <0901fumito@gmail.com>
2021-04-19 15:02:10 +0900

    itemモデルの単体テストコード実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,57 +6,103 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品出品の登録' do
-    
     context '商品出品の登録ができる時' do
       it '全てのフォームに入力があれば商品登録できる' do
+        expect(@item).to be_valid
       end
 
       it '販売価格が300円以上なら商品登録できる' do
+        @item.price = '300'
+        expect(@item).to be_valid
       end
 
       it '販売価格が9,999,999円以内なら商品登録できる' do
+        @item.price = '9999999'
+        expect(@item).to be_valid
       end
     end
 
     context '商品出品の登録ができない時' do
-
       it '商品画像が空だと登録できない' do
+        @item.image = nil 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
 
       it '商品名が空だと登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
 
       it '商品の説明が空だと登録できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
       end
 
       it 'カテゴリーの選択をしていないと登録できない' do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 0")
       end
 
       it '商品の状態を選択していないと登録できない' do
+        @item.condition_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 0")
+
       end
 
       it '配送料の負担を選択していないと登録できない' do
+        @item.postage_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage must be other than 0")
+
       end
 
       it '発送元の地域を選択していないと登録できない' do
+        @item.area_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area must be other than 0")
+
       end
 
       it '発送までの日数を選択していないと登録できない' do
+        @item.delivery_day_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day must be other than 0")
+
       end
 
       it '販売価格が空だと登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
       it '販売価格が299円以内だと登録できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
 
       it '販売価格が10,000,000円以上だと登録できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
 
       it '販売価格が半角数字以外では登録できない' do
+        @item.price = '１００００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
 
       it 'ユーザーログインしていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
