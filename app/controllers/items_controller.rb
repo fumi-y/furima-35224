48589176425e2fe69adeb76acce85c4f9b440843
fumi-y <0901fumito@gmail.com>
2021-04-20 11:05:26 +0900

    詳細ページ遷移の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_id, :area_id, :delivery_day_id, :price, ).merge(user_id: current_user.id)
