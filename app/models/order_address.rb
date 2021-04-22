d8a3bb82ae2c8de31c97f00a5422832236e6502b
fumi-y <0901fumito@gmail.com>
2021-04-22 12:39:50 +0900

    PAYJPを用いてクレジット決済機能の追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture, :municipality, :street_number, :building_name, :telephone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture, :municipality, :street_number, :building_name, :telephone_number, :token
 
   with_options presence: true do
     validates :user_id
@@ -10,6 +10,7 @@ class OrderAddress
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture, numericality: {other_than: 0}
     validates :telephone_number, format: { with: /\A\d{1,11}\z/ }
+    validates :token
   end
 
   def save
